fd53c348a611ed4125f1d4bf811ea9e39609ee68
clastajb <clastajb@fake.fake>
2023-04-24 13:04:56 +0200

    DEV-002_simple_dep_inject - @Component: constructor dependency injection

--- src/main/java/com/antsrl/formazione/simpledemo/Main.java
+++ src/main/java/com/antsrl/formazione/simpledemo/Main.java
@@ -1,7 +1,5 @@
 package com.antsrl.formazione.simpledemo;
 
-import com.antsrl.formazione.simpledemo.bean.Car;
-import com.antsrl.formazione.simpledemo.bean.Truck;
 import com.antsrl.formazione.simpledemo.config.AppConfig;
 import com.antsrl.formazione.simpledemo.config.EngineConfig;
 import com.antsrl.formazione.simpledemo.service.TransportService;
@@ -16,15 +14,6 @@ public class Main {
         context.register(EngineConfig.class);
         context.register(TransportService.class);
         context.refresh();
-
-        Car car = context.getBean(Car.class);
-        Truck truck = context.getBean(Truck.class);
-
-        car.engineStart();
-        car.go();
-
-        truck.engineStart();
-        truck.go();
 
         TransportService transportService = context.getBean(TransportService.class);
         transportService.movingStuffAroundTheWorld();
--- src/main/java/com/antsrl/formazione/simpledemo/service/TransportService.java
+++ src/main/java/com/antsrl/formazione/simpledemo/service/TransportService.java
@@ -1,15 +1,30 @@
 package com.antsrl.formazione.simpledemo.service;
 
+import com.antsrl.formazione.simpledemo.bean.Car;
+import com.antsrl.formazione.simpledemo.bean.Truck;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TransportService {
 
-    public TransportService() {
+    private final Car car;
+    private final Truck truck;
+
+    public TransportService(Car car, Truck truck) {
         System.out.println("preparing for transportation");
+
+        this.car = car;
+        this.truck = truck;
     }
 
     public void movingStuffAroundTheWorld(){
         System.out.println("hey, I'm moving stuff around!!");
+
+        car.engineStart();
+        car.go();
+
+        truck.engineStart();
+        truck.go();
+
     }
 }
