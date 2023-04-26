ed2519ed6791012cc2b1548a9ca7c9d8e45dc5cb
clastajb <clastajb@fake.fake>
2023-04-27 00:50:08 +0200

    DEV-003_simple_webapp - added Bean Validation on "save song" ( unmanaged result )

--- src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
+++ src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
@@ -2,6 +2,7 @@ package com.antsrl.formazione.springbasics.controller;
 
 import com.antsrl.formazione.springbasics.service.SongService;
 import com.antsrl.formazione.springbasics.uimodel.Song;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -31,7 +32,7 @@ public class MusicCatalogController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path="/")
-    public String addSong(@ModelAttribute Song song, Model model){
+    public String addSong(@Valid @ModelAttribute Song song, Model model){
 
         songService.save(song);
 
--- src/main/java/com/antsrl/formazione/springbasics/uimodel/Song.java
+++ src/main/java/com/antsrl/formazione/springbasics/uimodel/Song.java
@@ -1,12 +1,19 @@
 package com.antsrl.formazione.springbasics.uimodel;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
+import org.hibernate.validator.constraints.URL;
 
 @Builder
 public class Song {
 
     Long id;
+    @NotEmpty(message = "title is mandatory")
+    @Size(min = 3, max = 50, message = "title must be at least 3 and at most 50 characters long")
     String title;
+    @URL
     String url;
 
     public Long getId() {
