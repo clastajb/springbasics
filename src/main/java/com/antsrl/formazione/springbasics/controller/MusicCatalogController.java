0bd70caee2f73dee753b51a6ad938e82e780320a
clastajb <clastajb@fake.fake>
2023-04-27 00:53:59 +0200

    DEV-003_simple_webapp - avoid error page on Bean Validation ( still unmanaged result )

--- src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
+++ src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
@@ -6,6 +6,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -32,9 +33,11 @@ public class MusicCatalogController {
     }
 
     @RequestMapping(method = RequestMethod.POST, path="/")
-    public String addSong(@Valid @ModelAttribute Song song, Model model){
+    public String addSong(@Valid @ModelAttribute Song song, BindingResult bindingResult, Model model){
 
-        songService.save(song);
+        if(!bindingResult.hasErrors()){
+            songService.save(song);
+        }
 
         List<Song> songList = songService.getAllSongs();
 
