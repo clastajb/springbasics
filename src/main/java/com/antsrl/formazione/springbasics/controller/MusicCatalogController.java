2122d776f1d2d374a55b5824377f81f3959bcc20
clastajb <clastajb@fake.fake>
2023-04-26 20:24:41 +0200

    DEV-003_simple_webapp - thymeleaf iterating on a list

--- src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
+++ src/main/java/com/antsrl/formazione/springbasics/controller/MusicCatalogController.java
@@ -1,15 +1,40 @@
 package com.antsrl.formazione.springbasics.controller;
 
+import com.antsrl.formazione.springbasics.uimodel.Song;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class MusicCatalogController {
 
     @RequestMapping(method= RequestMethod.GET, path="/")
-    public String home(Model model){
+    public String listAllSongs(Model model){
+
+        List<Song> songList = Arrays.asList(
+                Song.builder()
+                        .id(41L)
+                        .title("Good Song")
+                        .url("http://example.com/good_song")
+                        .build(),
+                Song.builder()
+                        .id(23L)
+                        .title("Nice Song")
+                        .url("http://example.com/nice_song")
+                        .build(),
+                Song.builder()
+                        .id(98L)
+                        .title("Evil Song")
+                        .url("http://example.com/evil_song")
+                        .build()
+        );
+
+        model.addAttribute("songList", songList);
+
         return "music_catalog";
     }
 }
---
+++ src/main/java/com/antsrl/formazione/springbasics/uimodel/Song.java
@@ -0,0 +1,35 @@
+package com.antsrl.formazione.springbasics.uimodel;
+
+import lombok.Builder;
+
+@Builder
+public class Song {
+
+    Long id;
+    String title;
+    String url;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
