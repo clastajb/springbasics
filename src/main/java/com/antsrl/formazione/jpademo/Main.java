412ceb00ac5c743674df065b05afd3362e5342fd
clastajb <clastajb@fake.fake>
2023-05-01 16:24:37 +0200

    DEV-004_simple_jpa - starting point

---
+++ src/main/java/com/antsrl/formazione/jpademo/Main.java
@@ -0,0 +1,15 @@
+package com.antsrl.formazione.jpademo;
+
+import com.antsrl.formazione.jpademo.domain.Book;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Book book = new Book();
+
+        book.setId(34L);
+        book.setTitle("Good Romance");
+
+    }
+}
---
+++ src/main/java/com/antsrl/formazione/jpademo/domain/Book.java
@@ -0,0 +1,11 @@
+package com.antsrl.formazione.jpademo.domain;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Book {
+
+    private Long Id;
+    private String title;
+}
