522e98406776969f3e7d3b85a63f77e670c03194
clastajb <clastajb@fake.fake>
2023-05-01 18:29:44 +0200

    DEV-004_simple_jpa - create table and persist

--- src/main/java/com/antsrl/formazione/jpademo/Main.java
+++ src/main/java/com/antsrl/formazione/jpademo/Main.java
@@ -1,15 +1,45 @@
 package com.antsrl.formazione.jpademo;
 
 import com.antsrl.formazione.jpademo.domain.Book;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        try (EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("simple_jpa_demo");
+            EntityManager entityManager = entityManagerFactory.createEntityManager()) {
+
+            try{
+
+                entityManager.getTransaction().begin();
+
+                doPersistenceStuff(entityManager);
+
+                entityManager.getTransaction().commit();
+
+            } catch(Exception e){
+
+                entityManager.getTransaction().rollback();
+
+                e.printStackTrace();
+            }
+
+        } catch (Exception e){
+
+            e.printStackTrace();
+        }
+    }
+
+    private static void doPersistenceStuff(EntityManager entityManager) {
 
         Book book = new Book();
 
         book.setId(34L);
         book.setTitle("Good Romance");
 
+        entityManager.persist(book);
     }
 }
--- src/main/java/com/antsrl/formazione/jpademo/domain/Book.java
+++ src/main/java/com/antsrl/formazione/jpademo/domain/Book.java
@@ -1,11 +1,19 @@
 package com.antsrl.formazione.jpademo.domain;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+
+
+@Table(name = "book")
+@Entity
 @Getter
 @Setter
 public class Book {
 
+    @Id
     private Long Id;
     private String title;
 }
