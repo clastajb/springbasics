da503d657198134406a1093c1300f6444d6075e1
clastajb <clastajb@fake.fake>
2023-05-03 02:26:45 +0200

    DEV-005_simple_webapp - adding domain.Song JPA entity

---
+++ src/main/java/com/antsrl/formazione/springbasics/domain/Song.java
@@ -0,0 +1,22 @@
+package com.antsrl.formazione.springbasics.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    String title;
+
+    String url;
+}
